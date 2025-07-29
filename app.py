d10fa35bdbfd5317026080e21ac514865e2e2a90
gai-metaplanet <gai.metaplanet@gmail.com>
2025-07-29 23:02:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import yfinance
+import yfinance as yf
 import os
 
 st.title("My 3350 Trade History")
